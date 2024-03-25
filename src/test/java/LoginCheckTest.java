a4687e71c1e7e24f50c7b0d87d1589ac217bb02e
GitGudSoftEngDev <95184371+GitGudSoftEngDev@users.noreply.github.com>
2024-03-25 19:39:03 +0300

    Fix gsm

--- src/test/java/LoginCheckTest.java
+++ src/test/java/LoginCheckTest.java
@@ -17,7 +17,7 @@ public class LoginCheckTest {
 
     @Test
     public void login() {
-        Duration implicitWait = Duration.ofSeconds(5); // Например, 5 секунд
+        Duration implicitWait = Duration.ofSeconds(5); // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, 5 пїЅпїЅпїЅпїЅпїЅпїЅ
         driver.manage().window().maximize();
         driver.get("https://www.vk.com");
         WebElement username = driver.findElement(By.id("index_email"));
@@ -25,7 +25,6 @@ public class LoginCheckTest {
         WebElement button = driver.findElement(By.xpath("//button[@class='FlatButton FlatButton--primary FlatButton--size-l FlatButton--wide VkIdForm__button VkIdForm__signInButton']"));
         button.click();
         WebElement buttonContinue = (new WebDriverWait(driver, implicitWait))
-// или пока не появился элемент с id=bar
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class=\"vkc__DefaultSkin__button\"]")));
         buttonContinue.click();
     }
--- src/test/java/PiterGSMTest.java
+++ src/test/java/PiterGSMTest.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class PiterGSMTest {
 
@@ -13,9 +17,12 @@ public class PiterGSMTest {
     @Test
     public void login() {
         driver.manage().window().maximize();
+        Duration implicitWait = Duration.ofSeconds(5);
         driver.get("https://pitergsm.ru/");
+        WebElement buttonContinue = (new WebDriverWait(driver, implicitWait))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='city-popup__top']")));
         String city = driver.findElement(By.xpath("//div[@class='city-popup__top']")).getText();
-        Assert.assertEquals("Ваш город: Санкт-Петербург",city);
+        Assert.assertEquals("пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ: пїЅпїЅпїЅпїЅпїЅ-пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ",city);
         WebElement button = driver.findElement(By.xpath("//a[@class='sh-nav__link icon-user-white js-show-popup']"));
         button.click();
         WebElement login = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
@@ -25,7 +32,7 @@ public class PiterGSMTest {
         WebElement enter = driver.findElement(By.xpath("//input[@name='Login']"));
         enter.click();
         String errorMessage = driver.findElement(By.xpath("//font[@class='errortext']")).getText();
-        Assert.assertEquals("Неверный логин или пароль.", errorMessage);
+        Assert.assertEquals("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.", errorMessage);
     }
 
     @After
