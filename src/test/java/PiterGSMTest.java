ff267d4ce34f6278288633c576ef35dbf6a135ed
GitGudSoftEngDev <95184371+GitGudSoftEngDev@users.noreply.github.com>
2024-03-26 09:24:09 +0300

    Change of encoding

--- src/test/java/PiterGSMTest.java
+++ src/test/java/PiterGSMTest.java
@@ -22,7 +22,7 @@ public class PiterGSMTest {
         WebElement buttonContinue = (new WebDriverWait(driver, implicitWait))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='city-popup__top']")));
         String city = driver.findElement(By.xpath("//div[@class='city-popup__top']")).getText();
-        Assert.assertEquals("��� �����: �����-���������",city);
+        Assert.assertEquals("Ваш город: Санкт-Петербург",city);
         WebElement button = driver.findElement(By.xpath("//a[@class='sh-nav__link icon-user-white js-show-popup']"));
         button.click();
         WebElement login = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
@@ -32,7 +32,7 @@ public class PiterGSMTest {
         WebElement enter = driver.findElement(By.xpath("//input[@name='Login']"));
         enter.click();
         String errorMessage = driver.findElement(By.xpath("//font[@class='errortext']")).getText();
-        Assert.assertEquals("�������� ����� ��� ������.", errorMessage);
+        Assert.assertEquals("Неверный логин или пароль.", errorMessage);
     }
 
     @After
